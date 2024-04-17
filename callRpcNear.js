bedc0a0112f44ce4903b4be347b85123c4021b50
Wawan Darmawan <41707274+wanshade@users.noreply.github.com>
2024-04-17 10:00:03 +0800

    Update callRpcNear.js

--- callRpcNear.js
+++ callRpcNear.js
@@ -7,7 +7,7 @@ async function callRpcNear(url, count) {
       url,
       data: {
         jsonrpc: '2.0',
-        id: 'diluc',
+        id: 'tofg',
         method: 'block',
         params: { finality: 'final' },
       },
